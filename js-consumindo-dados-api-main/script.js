2a8c255a085a1fc8de64d003095c455aa0c4500b
leticiaSouzaDev <lec667121@gmail.com>
2024-06-19 14:42:47 -0300

    feat: customização dos erros

--- js-consumindo-dados-api-main/script.js
+++ js-consumindo-dados-api-main/script.js
@@ -1,4 +1,7 @@
 var consultaCep = fetch('https:viacep.com.br/ws/01001000/json/')// requisição a api com o fetch
     .then(resposta => resposta.json())// usado para transformar a resposta em json (then = então)
-    .then(r => console.log(r)) //usamos para mostrar a resposta da api (then = então)
-    .catch(erro => console.log(erro)); // usamos para tratamento de erro (caso a requisição(promise) ocorra um erro será exibida)
+    .then(r => {//usamos para mostrar a resposta da api (then = então)
+        if(r.erro) { // se o erro existir ->
+            throw Error('Esse cep não aexite!!');
+        } else console.log(r)})  
+     .catch(erro => console.log(erro)); // usamos para tratamento de erro (caso a requisição(promise) ocorra um erro será exibida)
