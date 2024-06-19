5190181723a5a3345bd3e4c3e35af68f13ce3b4d
leticiaSouzaDev <lec667121@gmail.com>
2024-06-19 14:33:58 -0300

    feat: tratando erro com o catch

--- js-consumindo-dados-api-main/script.js
+++ js-consumindo-dados-api-main/script.js
@@ -1,3 +1,4 @@
-var consultaCep = fetch('https:viacep.com.br/ws/01001000/json/')
-.then(resposta => resposta.json())// usado para transformar a resposta em json
-.then(r => console.log(r)) //usamos para mostrar a resposta da api
+var consultaCep = fetch('https:viacep.com.br/ws/01001000/json/')// requisição a api com o fetch
+    .then(resposta => resposta.json())// usado para transformar a resposta em json (then = então)
+    .then(r => console.log(r)) //usamos para mostrar a resposta da api (then = então)
+    .catch(erro => console.log(erro)); // usamos para tratamento de erro (caso a requisição(promise) ocorra um erro será exibida)
