a91e8356e15e5f1569b74dca97aed1aaa5547931
leticiaSouzaDev <lec667121@gmail.com>
2024-06-21 14:56:16 -0300

    feat: adicionando  promises All para realizar varias requisições

--- js-consumindo-dados-api-main/script.js
+++ js-consumindo-dados-api-main/script.js
@@ -1,15 +1,21 @@
-async function buscaEndereco(){
+async function buscaEndereco(cep){
     try{
-        var consultaCep = await fetch('https:viacep.com.br/ws/01001000/json/')// requisição a api com o fetch
+        var consultaCep = await fetch(`https:viacep.com.br/ws/${cep}/json/`)// requisição a api com o fetch
         var consultaCepConvertida = await consultaCep.json()// convertendo uma promisse para json
+        if(consultaCepConvertida.erro){
+            throw Error('Cep não existente!')
+        }
+
         console.log(consultaCepConvertida)
+        return consultaCepConvertida
     } 
     catch (erro){
         console.log(erro);
     }
 }
-
-buscaEndereco();
+let ceps = ['01001000', '01001001'];
+let conjuntoCeps = ceps.map(valores => buscaEndereco(valores))
+Promise.all(conjuntoCeps).then(respostas => console.log(respostas)) // usamos para fazser varias requisições
 
 
  
