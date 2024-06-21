22bc5d36e3e3e01920110d9bd4a686a05d318614
leticiaSouzaDev <lec667121@gmail.com>
2024-06-21 15:13:39 -0300

    feat: adicionando autocomplete nos campos de endereço com os dados da api encontrada

--- js-consumindo-dados-api-main/script.js
+++ js-consumindo-dados-api-main/script.js
@@ -1,21 +1,35 @@
 async function buscaEndereco(cep){
+    var mensagemErro = document.getElementById('erro');
+    mensagemErro.innerHTML = '';
     try{
         var consultaCep = await fetch(`https:viacep.com.br/ws/${cep}/json/`)// requisição a api com o fetch
         var consultaCepConvertida = await consultaCep.json()// convertendo uma promisse para json
         if(consultaCepConvertida.erro){
             throw Error('Cep não existente!')
         }
+        var cidade = document.getElementById('cidade');
+        var logradouro = document.getElementById('endereco')
+        var estado = document.getElementById('estado')
+
+        cidade.value = consultaCepConvertida.localidade; //adicionando o valor da api no campo apos o focusot
+        logradouro.value = consultaCepConvertida.logradouro;
+        estado.value = consultaCepConvertida.uf;
 
         console.log(consultaCepConvertida)
         return consultaCepConvertida
     } 
     catch (erro){
+        mensagemErro.innerHTML = `<p>CEo inválido. Tente novamente!</p>`
         console.log(erro);
     }
 }
-let ceps = ['01001000', '01001001'];
-let conjuntoCeps = ceps.map(valores => buscaEndereco(valores))
-Promise.all(conjuntoCeps).then(respostas => console.log(respostas)) // usamos para fazser varias requisições
+// let ceps = ['01001000', '01001001'];
+// let conjuntoCeps = ceps.map(valores => buscaEndereco(valores))
+// Promise.all(conjuntoCeps).then(respostas => console.log(respostas)) // usamos para fazser varias requisições
+
+var cep = document.getElementById('cep');
+cep.addEventListener("focusout", () => buscaEndereco(cep.value))
+
 
 
  
