df62f9de735ac687af317b9c16cbe6f555f66241
leticiaSouzaDev <lec667121@gmail.com>
2024-06-21 14:34:52 -0300

    feat: adicionando funcao assincrona e tratamento de erro

--- js-consumindo-dados-api-main/script.js
+++ js-consumindo-dados-api-main/script.js
@@ -1,7 +1,15 @@
-var consultaCep = fetch('https:viacep.com.br/ws/01001000/json/')// requisição a api com o fetch
-    .then(resposta => resposta.json())// usado para transformar a resposta em json (then = então)
-    .then(r => {//usamos para mostrar a resposta da api (then = então)
-        if(r.erro) { // se o erro existir ->
-            throw Error('Esse cep não aexite!!');
-        } else console.log(r)})  
-     .catch(erro => console.log(erro)); // usamos para tratamento de erro (caso a requisição(promise) ocorra um erro será exibida)
+async function buscaEndereco(){
+    try{
+        var consultaCep = await fetch('https:viacep.com.br/ws/01001000/json/')// requisição a api com o fetch
+        var consultaCepConvertida = await consultaCep.json()// convertendo uma promisse para json
+        console.log(consultaCepConvertida)
+    } 
+    catch (erro){
+        console.log(erro);
+    }
+}
+
+buscaEndereco();
+
+
+ 
